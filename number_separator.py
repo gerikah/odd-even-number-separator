0e8202745f74434f9d62ecde00bf6ffcf3bba0c5
gerikah <aldaygerikah@gmail.com>
2023-04-27 19:20:02 +0800

    open and read numbers.txt - initialize numbers

--- number_separator.py
+++ number_separator.py
@@ -1,5 +1,10 @@
+# Object Oriented Programming CMPE-103 PROGRAMMING EXERCISES : Problem 1
+# ALDAY, Gerikah L. - BSCPE 1-5
+
 # open the file "numbers.txt" in read mode
+with open("numbers.txt", "r") as input_file:
     # read all the integers from the file and convert them to a list of integers
+    numbers = list(map(int, input_file.read().split()))
 
 # create empty lists to store even and odd numbers
 # iterate over each number in the list of numbers
