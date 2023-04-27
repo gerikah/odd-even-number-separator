a1cc108e5aeaa652f4a03cbc9fd2faa4ed59a0bb
gerikah <aldaygerikah@gmail.com>
2023-04-27 19:21:43 +0800

    empty lists for even and odd numbers

--- number_separator.py
+++ number_separator.py
@@ -7,8 +7,10 @@ with open("numbers.txt", "r") as input_file:
     numbers = list(map(int, input_file.read().split()))
 
 # create empty lists to store even and odd numbers
+even_numbers = []
+odd_numbers = []
+
 # iterate over each number in the list of numbers
-    # check if the number is even or odd
         # add the even number to the list of even numbers
         # add the odd number to the list of odd numbers
 # write the even numbers to the file "even.txt"
