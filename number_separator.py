e67e481121279389799070aa06b0d90a8a74c4ab
gerikah <aldaygerikah@gmail.com>
2023-04-27 19:06:32 +0800

    pseudocode and txt files

---
+++ number_separator.py
@@ -0,0 +1,10 @@
+# open the file "numbers.txt" in read mode
+    # read all the integers from the file and convert them to a list of integers
+
+# create empty lists to store even and odd numbers
+# iterate over each number in the list of numbers
+    # check if the number is even or odd
+        # add the even number to the list of even numbers
+        # add the odd number to the list of odd numbers
+# write the even numbers to the file "even.txt"
+# write the odd numbers to the file "odd.txt"
